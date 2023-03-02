63a999b52d832881c917e4f77a039f5d410d5d36
Chriscodor <115654875+Chriscodor@users.noreply.github.com>
2023-03-02 18:48:55 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -22,3 +22,18 @@ function modelLoaded(){
     console.log("yayy model is loaded")
 }
 
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results)
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
